5a47bf2bdb562b46ab97946646c52e4455674c75
yamanobori_old <kazunori.ifuku@gmail.com>
2012-03-04 00:18:39 +0900

    関数化した

--- api.c
+++ api.c
@@ -12,44 +12,38 @@ static void raw_putchars(API_PARAM *param)
     Sci_PutChars(param->pm_putchars.param);
     param->result = 0;
 }
+static int execute(API_PARAM *param)
+{
+    API_PARAM *res;
+
+    get_tid(&param->id);
+
+    snd_mbx(MID_WORKER,(T_MSG *)param);
+    slp_tsk();
+
+    rcv_mbx(MID_APP,(T_MSG **)&res);
+    return res->result;
+}
 unsigned int api_twice(int ap)
 {
 
     API_PARAM param;
-    API_PARAM *res;
-    int result;
 
     param.api = API_TWICE;
     param.pm_twice.param = ap;
-    get_tid(&param.id);
 
-    snd_mbx(MID_WORKER,(T_MSG *)&param);
-    slp_tsk();
-
-    rcv_mbx(MID_APP,(T_MSG **)&res);
-    result = res->result;
-
-    return result;
+    return execute(&param);
 }
 
 unsigned int api_putchars(const char* str)
 {
 
     API_PARAM param;
-    API_PARAM *res;
-    int result;
 
     param.api = API_PUTCHARS;
     param.pm_putchars.param = str;
-    get_tid(&param.id);
 
-    snd_mbx(MID_WORKER,(T_MSG *)&param);
-    slp_tsk();
-
-    rcv_mbx(MID_APP,(T_MSG **)&res);
-    result = res->result;
-
-    return result;
+    return execute(&param);
 }
 
 
